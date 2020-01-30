3d66404c75fed8ef1e86e1c4d38190647607cff8
Chad Carbert <chadcarbert@me.com>
2020-01-30 13:51:46 -0500

    Update todo_list with auto-incrementing ids

--- chapter-4/todo_list.ex
+++ chapter-4/todo_list.ex
@@ -1,26 +1,29 @@
 # page 105
 
 defmodule TodoList do
-  def new(), do: MultiDict.new()
+  defstruct auto_id: 1, entries: %{}
+
+  def new(), do: %TodoList{}
 
   def add_entry(todo_list, item) do
-    MultiDict.add(todo_list, item.date, item)
+    # MultiDict.add(todo_list, item.date, item)
+
+    # add the id to the entry
+    entry = Map.put(item, :id, todo_list.auto_id)
+
+    # create new entries
+    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
+
+    # add entries to new todo_list
+    new_todo_list = Map.put(todo_list, :entries, new_entries)
+      # and increment the id
+      |> Map.put(:auto_id, todo_list.auto_id + 1)
+
+    new_todo_list
   end
 
   def entries(todo_list, date) do
     MultiDict.get(todo_list, date)
-  end
-end
-
-defmodule MultiDict do
-  def new(), do: %{}
-
-  def add(dict, key, value) do
-    Map.update(dict, key, [value], fn previous -> [value | previous] end)
-  end
-
-  def get(dict, key) do
-    Map.get(dict, key)
   end
 end
 
@@ -35,7 +38,7 @@ defmodule Test do
   end
 end
 
-Test.it("can add and retrieve entries by date", fn() ->
+Test.it("can add entries with an incrementing id", fn() ->
   todo_list =
     TodoList.new() |>
       TodoList.add_entry(%{
@@ -45,25 +48,48 @@ Test.it("can add and retrieve entries by date", fn() ->
       TodoList.add_entry(%{
         title: "Shopping",
         date: ~D[2018-12-20]
-      }) |>
-      TodoList.add_entry(%{
-        title: "Movies",
-        date: ~D[2018-12-19]
       })
 
-  IO.inspect(todo_list)
+  Test.equals(Map.get(todo_list.entries, 1), %{
+    id: 1,
+    title: "Dentist",
+    date: ~D[2018-12-19]
+  })
 
-  Test.equals(
-    TodoList.entries(todo_list, ~D[2018-12-19]),
-    [
-      %{
-        title: "Movies",
-        date: ~D[2018-12-19]
-      },
-      %{
-        title: "Dentist",
-        date: ~D[2018-12-19]
-      }
-    ]
-  )
+  Test.equals(Map.get(todo_list.entries, 2), %{
+    id: 2,
+    title: "Shopping",
+    date: ~D[2018-12-20]
+  })
 end)
+
+# Test.it("can add and retrieve entries by date", fn() ->
+#   todo_list =
+#     TodoList.new() |>
+#       TodoList.add_entry(%{
+#         title: "Dentist",
+#         date: ~D[2018-12-19]
+#       }) |>
+#       TodoList.add_entry(%{
+#         title: "Shopping",
+#         date: ~D[2018-12-20]
+#       }) |>
+#       TodoList.add_entry(%{
+#         title: "Movies",
+#         date: ~D[2018-12-19]
+#       })
+
+#   Test.equals(
+#     TodoList.entries(todo_list, ~D[2018-12-19]),
+#     [
+#       %{
+#         title: "Movies",
+#         date: ~D[2018-12-19]
+#       },
+#       %{
+#         title: "Dentist",
+#         date: ~D[2018-12-19]
+#       }
+#     ]
+#   )
+# end)
